241ea6bffa6cabe74cfbd344f5ed8c525081e77c
jonny sywulak <jonny@stelligent.com>
2019-03-20 17:55:41 -0400

    removes unneeded code

--- lambda/send_notification.py
+++ lambda/send_notification.py
@@ -10,36 +10,6 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     logger.info('REQUEST RECEIVED:\n {}'.format(event))
     logger.info('REQUEST RECEIVED:\n {}'.format(context))
-    code_pipeline = boto3.client('codepipeline')
-    job = event['CodePipeline.job']['id']
-    logger.info('jobid = {}'.format(job))
-    # take approval notification token
-
-
-
-    code_pipeline.put_job_success_result(jobId=job)
-
-
-def sendResponse(event, context, responseStatus, responseData):
-    responseBody = json.dumps({
-        "Status": responseStatus,
-        "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
-        "PhysicalResourceId": context.log_stream_name,
-        "StackId": event['StackId'],
-        "RequestId": event['RequestId'],
-        "LogicalResourceId": event['LogicalResourceId'],
-        "Data": responseData
-    })
-
-
-    logger.info('ResponseURL: {}'.format(event['ResponseURL']))
-    logger.info('ResponseBody: {}'.format(responseBody))
-
-    opener = build_opener(HTTPHandler)
-    request = Request(event['ResponseURL'], data=responseBody)
-    request.add_header('Content-Type', '')
-    request.add_header('Content-Length', len(responseBody))
-    request.get_method = lambda: 'PUT'
-    response = opener.open(request)
-    print("Status code: {}".format(response.getcode()))
-    print("Status message: {}".format(response.msg))
+    # determine name of sns topic
+    # create sns client
+    # send sns message
