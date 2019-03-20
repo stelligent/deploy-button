94c3e883527a09b60fb675f3eb54274620869979
jonny sywulak <jonny@stelligent.com>
2019-03-20 17:56:37 -0400

    removed unneeded code

--- lambda/receive_button_press.py
+++ lambda/receive_button_press.py
@@ -37,27 +37,3 @@ def handler(event, context):
         token=token
     )
     logger.info('response:\n {}'.format(response))
-
-
-def sendResponse(event, context, responseStatus, responseData):
-    responseBody = json.dumps({
-        "Status": responseStatus,
-        "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
-        "PhysicalResourceId": context.log_stream_name,
-        "StackId": event['StackId'],
-        "RequestId": event['RequestId'],
-        "LogicalResourceId": event['LogicalResourceId'],
-        "Data": responseData
-    })
-
-    logger.info('ResponseURL: {}'.format(event['ResponseURL']))
-    logger.info('ResponseBody: {}'.format(responseBody))
-
-    opener = build_opener(HTTPHandler)
-    request = Request(event['ResponseURL'], data=responseBody)
-    request.add_header('Content-Type', '')
-    request.add_header('Content-Length', len(responseBody))
-    request.get_method = lambda: 'PUT'
-    response = opener.open(request)
-    print("Status code: {}".format(response.getcode()))
-    print("Status message: {}".format(response.msg))
